48fdead0f25ed90f1e3dba72a264ef6802793633
Jomiszcz <jomiszcz@wp.pl>
2025-06-29 08:31:03 +0200

    Create server.js

---
+++ node/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+// console.log(http);
+const port = 3000
+
+http.createServer((req, res) => {
+
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        res.write('<h1>Strona główna</h1>');
+        res.end();
+    } else if (req.url === '/form') {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        res.write('<h1>Formularz rejestracyjny</h1>');
+        res.end();
+    }
+
+}).listen(port, () => {
+    console.log(`Serwer działa i nasłuchuje na porcie ${port}`);
+})
